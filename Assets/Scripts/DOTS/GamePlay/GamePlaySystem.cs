d47d0368e758c8250e99c833cffd6058c38e08da
Efaideleon <efaideleon@gmail.com>
2025-02-26 11:23:25 -0800

    erorr

--- Assets/Scripts/DOTS/GamePlay/GamePlaySystem.cs
+++ Assets/Scripts/DOTS/GamePlay/GamePlaySystem.cs
@@ -1,5 +1,4 @@
 using Unity.Entities;
-using UnityEngine;
 
 public partial struct GamePlaySystem : ISystem
 {
@@ -10,9 +9,8 @@ public partial struct GamePlaySystem : ISystem
 
     public void OnUpdate(ref SystemState state)
     {
-        foreach (var (turnComponent, nameComponet, entity) in SystemAPI.Query<RefRW<TurnComponent>, RefRO<NameDataComponent>>().WithEntityAccess())
-        {
-            Debug.Log($"Name: { nameComponet.ValueRO.Name } Current Flag: { turnComponent.ValueRO.IsCurrentActivePlayer }");
-        }
+        /*foreach (var (turnComponent, nameComponet, entity) in SystemAPI.Query<RefRW<TurnComponent>, RefRO<NameDataComponent>>().WithEntityAccess())*/
+        /*{*/
+        /*}*/
     }
 }
--- Assets/Scripts/DOTS/UI/GameUICanvasSystem.cs
+++ Assets/Scripts/DOTS/UI/GameUICanvasSystem.cs
@@ -1,37 +1,43 @@
-using System.IO.Pipes;
-using Unity.Collections;
 using Unity.Entities;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public partial struct GameUICanvasSystem : ISystem
 {
+    VisualElement gameScreenTopRoot;
+    VisualElement gameScreenBotRoot;
+    VisualElement rollContainer;
+    VisualElement youBoughtContainer;
+    VisualElement taxContainer;
+    VisualElement jailContainer;
+    VisualElement goToJailContainer;
+    VisualElement chanceContainer;
+    VisualElement goContainer;
+    VisualElement parkingContainer;
+    VisualElement treasureContainer;
+    Label playerNameLabel;
+
     public void OnCreate(ref SystemState state)
     {
         state.RequireForUpdate<CanvasReferenceComponent>();
-    }
-
-    public void OnUpdate(ref SystemState state)
-    {
-        state.Enabled = false;
         var canvasReference = SystemAPI.ManagedAPI.GetSingleton<CanvasReferenceComponent>();
 
         var uiDocument = Object.Instantiate(canvasReference.uiDocumentGO).GetComponent<UIDocument>();
 
-        VisualElement gameScreenTopRoot = uiDocument.rootVisualElement.Q<VisualElement>("game-screen-top-container");
-        VisualElement gameScreenBotRoot = uiDocument.rootVisualElement.Q<VisualElement>("game-screen-bottom-container");
+        gameScreenTopRoot = uiDocument.rootVisualElement.Q<VisualElement>("game-screen-top-container");
+        gameScreenBotRoot = uiDocument.rootVisualElement.Q<VisualElement>("game-screen-bottom-container");
 
-        Label playerNameLabel = gameScreenTopRoot.Q<Label>("player-name-label");
+        rollContainer = gameScreenBotRoot.Q<VisualElement>("roll-display-container");
+        youBoughtContainer = gameScreenBotRoot.Q<VisualElement>("you-bought-display-container");
+        taxContainer = gameScreenBotRoot.Q<VisualElement>("tax-display-container");
+        jailContainer = gameScreenBotRoot.Q<VisualElement>("jail-display-container");
+        goToJailContainer = gameScreenBotRoot.Q<VisualElement>("go-to-jail-display-container");
+        chanceContainer = gameScreenBotRoot.Q<VisualElement>("chance-display-container");
+        goContainer = gameScreenBotRoot.Q<VisualElement>("go-display-container");
+        parkingContainer = gameScreenBotRoot.Q<VisualElement>("parking-display-container");
+        treasureContainer = gameScreenBotRoot.Q<VisualElement>("treasure-display-container");
 
-        VisualElement rollContainer = gameScreenBotRoot.Q<VisualElement>("roll-display-container");
-        VisualElement youBoughtContainer = gameScreenBotRoot.Q<VisualElement>("you-bought-display-container");
-        VisualElement taxContainer = gameScreenBotRoot.Q<VisualElement>("tax-display-container");
-        VisualElement jailContainer = gameScreenBotRoot.Q<VisualElement>("jail-display-container");
-        VisualElement goToJailContainer = gameScreenBotRoot.Q<VisualElement>("go-to-jail-display-container");
-        VisualElement chanceContainer = gameScreenBotRoot.Q<VisualElement>("chance-display-container");
-        VisualElement goContainer = gameScreenBotRoot.Q<VisualElement>("go-display-container");
-        VisualElement parkingContainer = gameScreenBotRoot.Q<VisualElement>("parking-display-container");
-        VisualElement treasureContainer = gameScreenBotRoot.Q<VisualElement>("treasure-display-container");
+        playerNameLabel = gameScreenTopRoot.Q<Label>("player-name-label");
 
         rollContainer.style.display = DisplayStyle.None;
         youBoughtContainer.style.display = DisplayStyle.None;
@@ -42,7 +48,10 @@ public partial struct GameUICanvasSystem : ISystem
         goContainer.style.display = DisplayStyle.None;
         parkingContainer.style.display = DisplayStyle.None;
         treasureContainer.style.display = DisplayStyle.None;
+    }
 
+    public void OnUpdate(ref SystemState state)
+    {
         foreach (var (turnComponent, nameComponent) in SystemAPI.Query<RefRO<TurnComponent>, RefRO<NameDataComponent>>())
         {
             if (turnComponent.ValueRO.IsCurrentActivePlayer)
