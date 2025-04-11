23c07d948624e1f20b002dc89b0c24a54a692f38
Efaideleon <efaideleon@gmail.com>
2025-04-11 13:36:33 -0700

    minor changes

--- Assets/Scripts/DOTS/UI/BuyHouseUIController.cs
+++ Assets/Scripts/DOTS/UI/BuyHouseUIController.cs
@@ -1,3 +1,11 @@
+using Assets.Scripts.DOTS.UI.UIPanels;
+
 public class BuyHouseUIController
 {
+    public BuyHouseUI BuyHouseUI { get; private set; }
+
+    public BuyHouseUIController(BuyHouseUI buyHouseUI)
+    {
+        BuyHouseUI = buyHouseUI;
+    }
 }
