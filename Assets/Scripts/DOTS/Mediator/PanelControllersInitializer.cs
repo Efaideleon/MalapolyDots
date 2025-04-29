3cf027ac647d1218416c1259c0f779298556bb2d
Efaideleon <efaideleon@gmail.com>
2025-04-29 12:20:26 -0700

    big player container

--- Assets/Scripts/DOTS/Mediator/PanelControllersInitializer.cs
+++ Assets/Scripts/DOTS/Mediator/PanelControllersInitializer.cs
@@ -68,10 +68,13 @@ namespace DOTS.Mediator
             var botPanelRoot = uiDocument.rootVisualElement.Q<VisualElement>("game-screen-bottom-container");
             var backdrop = uiDocument.rootVisualElement.Q<Button>("Backdrop");
             var playerNameMoneyContainer = botPanelRoot.Q<VisualElement>("PlayersStatsContainer");
+            var bigPlayerNameMoneyContainer = botPanelRoot.Q<VisualElement>("current-player-container");
 
             if (playerNameMoneyContainer == null)
                 return;
 
+            if (bigPlayerNameMoneyContainer == null)
+                return;
 
             PurchasePropertyPanelContext purchasePropertyPanelContext = new() { Name = default, Price = default, };
             PurchaseHousePanelContext purchaseHousePanelContext = new() { Name = default, HousesOwned = default, Price = default };
@@ -115,7 +118,7 @@ namespace DOTS.Mediator
             }
 
             // Instantiating the Stats Panel for each player
-            panelControllers.statsPanelController = new(playerNameMoneyContainer, new StatsPanelContext());
+            panelControllers.statsPanelController = new(playerNameMoneyContainer, bigPlayerNameMoneyContainer, new StatsPanelContext());
             foreach (var characterBuffer in SystemAPI.Query<DynamicBuffer<CharacterSelectedNameBuffer>>())
             {
                 foreach (var character in characterBuffer)
