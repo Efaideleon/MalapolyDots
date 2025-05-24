1208a70321c0dfe3b5dd97b3e1c47d09c98e6e12
Efaideleon <efaideleon@gmail.com>
2025-05-23 19:44:02 -0700

    coloring properties after purchasing them

--- Assets/Scripts/DOTS/GamePlay/PurchasePropertyColoringSystem.cs
+++ Assets/Scripts/DOTS/GamePlay/PurchasePropertyColoringSystem.cs
@@ -1,13 +1,51 @@
+using DOTS.Constants;
+using DOTS.DataComponents;
+using DOTS.GameSpaces;
+using Unity.Burst;
 using Unity.Entities;
 
 namespace DOTS.GamePlay
 {
-    public partial struct PurchasePropertyColorSystem : IComponentData
+    [BurstCompile]
+    public partial struct PurchasePropertyColorSystem : ISystem
     {
-        public void OnCreate(ref SystemState state)
-        {}
+        private const float coloringSpeed = 2f;
+        private const int ColoringThreshold = 10;
 
+        [BurstCompile]
+        public void OnCreate(ref SystemState state)
+        {
+            state.RequireForUpdate<MaterialOverrideColorSlider>();
+        }
+
+        [BurstCompile]
         public void OnUpdate(ref SystemState state)
-        {}
+        {
+            var dt = SystemAPI.Time.DeltaTime;
+            foreach (var (materialColorSlider, owner, _) in 
+                    SystemAPI.Query<
+                        RefRW<MaterialOverrideColorSlider>,
+                        RefRO<OwnerComponent>,
+                        RefRO<PropertySpaceTag>
+                    >())
+            {
+                var colorSliderRO = materialColorSlider.ValueRO;
+                bool coloring = colorSliderRO.Value < ColoringThreshold;
+
+                if (coloring)
+                {
+                    var ownerID = owner.ValueRO.ID;
+                    ref var colorSliderRW = ref materialColorSlider.ValueRW;
+
+                    bool purchased = ownerID != PropertyConstants.Vacant;
+
+                    if  (purchased)
+                        colorSliderRW.Value = colorSliderRO.Value + coloringSpeed * dt;
+
+                    if (colorSliderRO.Value >= ColoringThreshold)
+                        colorSliderRW.Value = ColoringThreshold;
+                }
+            }
+        }
     }
 }
