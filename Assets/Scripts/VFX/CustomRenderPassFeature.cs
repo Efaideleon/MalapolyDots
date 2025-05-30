085ea303c1fb8abf7ac0ed0586be075e5e77e298
Efaideleon <efaideleon@gmail.com>
2025-05-30 13:24:20 -0700

    idk

--- Assets/Scripts/VFX/CustomRenderPassFeature.cs
+++ Assets/Scripts/VFX/CustomRenderPassFeature.cs
@@ -1,16 +1,36 @@
-using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 using UnityEngine.Rendering.RenderGraphModule;
+using UnityEngine;
 
 public class CustomRenderPassFeature : ScriptableRendererFeature
 {
     class CustomRenderPass : ScriptableRenderPass
     {
+        private LayerMask _layerMask;
+
+        public CustomRenderPass(LayerMask layerMask)
+        {
+            _layerMask = layerMask;
+        }
         // This class stores the data needed by the RenderGraph pass.
         // It is passed as a parameter to the delegate function that executes the RenderGraph pass.
         private class PassData
         {
+            public RendererListHandle rendererListHandle;
+        }
+
+        private void InitRendererLists(ContextContainer frameData, ref PassData passData, RenderGraph renderGraph)
+        {
+            UniversalRenderingData universalRenderingData = frameData.Get<UniversalRenderingData>();
+            UniversalCameraData cameraData = frameData.Get<UniversalCameraData>();
+
+            RenderQueueRange renderQueueRange = new();
+            FilteringSettings filteringSettings = new(renderQueueRange, _layerMask);
+            DrawingSettings drawingSettings = new();
+
+            RendererListParams param = new(universalRenderingData.cullResults, drawingSettings, filteringSettings);
+            passData.rendererListHandle = renderGraph.CreateRendererList(param);
         }
 
         // This static method is passed as the RenderFunc delegate to the RenderGraph render pass.
@@ -36,11 +56,15 @@ public class CustomRenderPassFeature : ScriptableRendererFeature
                 // UniversalCameraData cameraData = frameData.Get<UniversalCameraData>();
                 UniversalResourceData resourceData = frameData.Get<UniversalResourceData>();
 
+                InitRendererLists(frameData, ref passData, renderGraph);
+
+                builder.UseRendererList(passData.rendererListHandle);
+
                 // Setup pass inputs and outputs through the builder interface.
                 // Eg:
                 // builder.UseTexture(sourceTexture);
                 // TextureHandle destination = UniversalRenderer.CreateRenderGraphTexture(renderGraph, cameraData.cameraTargetDescriptor, "Destination Texture", false);
-                
+
                 // This sets the render target of the pass to the active color texture. Change it to your own render target as needed.
                 builder.SetRenderAttachment(resourceData.activeColorTexture, 0);
 
@@ -48,39 +72,16 @@ public class CustomRenderPassFeature : ScriptableRendererFeature
                 builder.SetRenderFunc((PassData data, RasterGraphContext context) => ExecutePass(data, context));
             }
         }
-
-        // NOTE: This method is part of the compatibility rendering path, please use the Render Graph API above instead.
-        // This method is called before executing the render pass.
-        // It can be used to configure render targets and their clear state. Also to create temporary render target textures.
-        // When empty this render pass will render to the active camera render target.
-        // You should never call CommandBuffer.SetRenderTarget. Instead call <c>ConfigureTarget</c> and <c>ConfigureClear</c>.
-        // The render pipeline will ensure target setup and clearing happens in a performant manner.
-        public override void OnCameraSetup(CommandBuffer cmd, ref RenderingData renderingData)
-        {
-        }
-
-        // NOTE: This method is part of the compatibility rendering path, please use the Render Graph API above instead.
-        // Here you can implement the rendering logic.
-        // Use <c>ScriptableRenderContext</c> to issue drawing commands or execute command buffers
-        // https://docs.unity3d.com/ScriptReference/Rendering.ScriptableRenderContext.html
-        // You don't have to call ScriptableRenderContext.submit, the render pipeline will call it at specific points in the pipeline.
-        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
-        {
-        }
-
-        // NOTE: This method is part of the compatibility rendering path, please use the Render Graph API above instead.
-        // Cleanup any allocated resources that were created during the execution of this render pass.
-        public override void OnCameraCleanup(CommandBuffer cmd)
-        {
-        }
     }
+
+    public LayerMask _layerMask;
 
     CustomRenderPass m_ScriptablePass;
 
     /// <inheritdoc/>
     public override void Create()
     {
-        m_ScriptablePass = new CustomRenderPass();
+        m_ScriptablePass = new CustomRenderPass(_layerMask);
 
         // Configures where the render pass should be injected.
         m_ScriptablePass.renderPassEvent = RenderPassEvent.AfterRenderingOpaques;
