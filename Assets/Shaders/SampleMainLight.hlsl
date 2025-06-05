4668a9d6020314bcd3083c27bd2186814f2d9933
abeledl <132021571+abeledl@users.noreply.github.com>
2025-06-05 16:53:56 -0700

    bor

--- Assets/Shaders/SampleMainLight.hlsl
+++ Assets/Shaders/SampleMainLight.hlsl
@@ -1,33 +1,15 @@
-#ifndef SAMPLE_MAIN_LIGHT_INCLUDED
-#define SAMPLE_MAIN_LIGHT_INCLUDED
 
-#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
-#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
-
-void SampleAllLightsFcn_float(float3 worldPos, float3 worldNormal, out float3 lightColor)
+void GetLight_float(float3 WorldPos, out float3 Direction, out float3 Color, out float Attenuation)
 {
-    // Initialize output
-    lightColor = float3(0, 0, 0);
-    
-    // Sample main light
-    float4 shadowCoord = TransformWorldToShadowCoord(worldPos);
-    Light mainLight = GetMainLight(shadowCoord);
-    
-    float3 lightDir = normalize(mainLight.direction);
-    float NdotL = saturate(dot(worldNormal, lightDir));
-    
-    lightColor += mainLight.color * NdotL * mainLight.distanceAttenuation * mainLight.shadowAttenuation;
-    
-    // Sample additional lights
-    uint pixelLightCount = GetAdditionalLightsCount();
-    
-    LIGHT_LOOP_BEGIN(pixelLightCount)
-        Light light = GetAdditionalLight(lightIndex, worldPos);
-        
-    float3 addLightDir = normalize(light.direction);
-    float addNdotL = saturate(dot(worldNormal, addLightDir));
-        
-    lightColor += light.color * addNdotL * light.distanceAttenuation * light.shadowAttenuation;
-    LIGHT_LOOP_END
-}
+#if defined(SHADERGRAPH_PREVIEW)
+    Direction = float3(0.5, 0.5, 0);
+    Color = 1;
+    Attenuation = 0.5;
+#else 
+    float4 sCoord = TransformWorldToShadowCoord(WorldPos);
+    Light mainLight = GetMainLight(sCoord);
+    Direction = mainLight.direction;
+    Color = mainLight.color;
+    Attenuation = mainLight.shadowAttenuation;
 #endif
+}
