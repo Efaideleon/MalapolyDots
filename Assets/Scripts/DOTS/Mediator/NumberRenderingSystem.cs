c3b50f933caf5b0305f2439ff145ae2ee6bd55c6
Efaideleon <efaideleon@gmail.com>
2025-12-11 16:34:25 -0800

    idk

--- Assets/Scripts/DOTS/Mediator/NumberRenderingSystem.cs
+++ Assets/Scripts/DOTS/Mediator/NumberRenderingSystem.cs
@@ -11,6 +11,7 @@ namespace DOTS.Mediator
 
         public void OnCreate(ref SystemState state)
         {
+            state.RequireForUpdate<AssetsMaterial>();
             state.RequireForUpdate<QuadDataBuffer>();
         }
 
@@ -27,16 +28,50 @@ namespace DOTS.Mediator
             // }
             //
             var assets = SystemAPI.ManagedAPI.GetSingleton<AssetsMaterial>();
+
+            var entity = SystemAPI.GetSingletonEntity<QuadDataBuffer>();
+            var pos = SystemAPI.GetComponent<LocalToWorld>(entity).Position;
+
+            QuadData q1 = new() { UV0 = new float2(0, 0), UV1 = new float2(0.5f, 1f) };
+            QuadData q2 = new() { UV0 = new float2(0, 0.5f), UV1 = new float2(1, 1) };
+
+            var block1 = new MaterialPropertyBlock();
+            var block2 = new MaterialPropertyBlock();
+
+            block1.SetVector("_UV0", new Vector4(q1.UV0.x, q1.UV0.y, 0, 0));
+            block1.SetVector("_UV1", new Vector4(q1.UV1.x, q1.UV1.y, 0, 0));
+
+            block2.SetVector("_UV0", new Vector4(q2.UV0.x, q2.UV0.y, 0, 0));
+            block2.SetVector("_UV1", new Vector4(q2.UV1.x, q2.UV1.y, 0, 0));
+
             Graphics.DrawMesh
             (
                 assets.mesh,
-                new Vector3(0, 0, 0),
-                quaternion.identity,
+                float4x4.Translate(pos),
                 assets.material,
-                0
+                0,
+                null,
+                0,
+                block1
             );
 
-            state.Enabled = false;
+            var offset = new float3(0.2f, 0, 0);
+            Graphics.DrawMesh
+            (
+                assets.mesh,
+                float4x4.Translate(pos + offset),
+                assets.material,
+                0,
+                null,
+                0,
+                block2
+            );
         }
+    }
+
+    public struct QuadData
+    {
+        public float2 UV0;
+        public float2 UV1;
     }
 }
