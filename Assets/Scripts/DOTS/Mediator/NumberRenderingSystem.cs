68c3a75f4e9133008d3074a4b7ad48f3cbc3a3c0
Efaideleon <efaideleon@gmail.com>
2025-12-12 12:06:15 -0800

    900

--- Assets/Scripts/DOTS/Mediator/NumberRenderingSystem.cs
+++ Assets/Scripts/DOTS/Mediator/NumberRenderingSystem.cs
@@ -37,7 +37,7 @@ namespace DOTS.Mediator
             var renderMeshArray = new RenderMeshArray(new[] { assets.material }, new[] { assets.mesh });
 
 
-            for (int i = 0; i < 100; i++)
+            for (int i = 0; i < 900; i++)
             {
                 var quadEntity = state.EntityManager.CreateEntity();
                 RenderMeshUtility.AddComponents(
